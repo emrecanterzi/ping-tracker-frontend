52cef92f75e79a77999a687a4c7815ec57e0f917
emrecanterzi <terzi.6112@gmail.com>
2023-08-04 16:07:33 +0300

    #17 store  updated

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,5 +7,5 @@ export const store = configureStore({
   },
 });
 
-export type IRootState = ReturnType<typeof store.getState>;
-export type IAppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
